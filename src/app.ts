609ab9e81552f96922866c4640ffeaa70aca7a1e
magszho <113620086+magszho@users.noreply.github.com>
2025-01-22 22:16:00 -0500

    backend start

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.get("/test", (req: Request, res: Response) => {
+    res.send("here's a silly message");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'website.html'));
+});
+
+
+app.listen(port, () => {
+console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+export default app;
