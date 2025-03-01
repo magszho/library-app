600dee955518f906931edb7f6c5c2cabd4f77499
magszho <maggiezhou878@gmail.com>
2025-03-01 13:08:17 -0500

    can search for all users by email

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -22,17 +22,28 @@ try {
 }
 });
 
-userRouter.get('/users', async (req: Request<{}, {}, {}, {email: string}>, res: Response): Promise<void> => {
-    const { email } = req.query;
+userRouter.get('/users', async (req: Request<{}, {}, {}, {email?: string}>, res: Response): Promise<void> => {
+    // const { email } = req.query;
+    // try {
+    //     const user = await User.findOne({ email: email }).exec();
+    //     if (!user) {
+    //         res.status(404).send({ message: 'User not found' });
+    //             return;
+    //     }
+    //     res.status(200).json(user);
+    // } catch (error) {
+    //     res.status(500).send({ error: 'Error occurred while finding user' });
+    // }
+
     try {
-        const user = await User.findOne({ email: email }).exec();
-        if (!user) {
-            res.status(404).send({ message: 'User not found' });
-                return;
+        let query = {};
+        if (req.query.email) {
+            query = { email: req.query.email };
         }
-        res.status(200).json(user);
+        const users = await User.find(query).exec();
+        res.status(200).json(users);
     } catch (error) {
-        res.status(500).send({ error: 'Error occurred while finding user' });
+        res.status(400).json({ error: 'FError ocurred while finding user' });
     }
 });
 
